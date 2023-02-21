78711311e003f36e289c0bbc59fd7bca4333866b
Onkarabile <letlapeonka@gmail.com>
2023-02-21 15:59:37 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main- print alphbet 10x
+ * Return: 0 success
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+      	int i;
+
+  i =0;
+  while(i<10)
+  {
+	  letter = 'a';
+	  while(letter < z)
+	  {
+		_putchar(letter);
+		letter++;
+	  }
+ _putchar('\n');
+ i++;
+  }
+}
