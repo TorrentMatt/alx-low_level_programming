c54141ecba257890fef0baa2f1572b725cbae163
Onkarabile <letlapeonka@gmail.com>
2023-02-23 12:23:28 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiply 2 intgers
+ * @a: a - Variable
+ * @b: b - variable
+ * Return(0)
+ */
+int mul(int a, int b)
+{
+	int mul;
+	mul =a *b;
+	return (mul);
+}
