3e61182051c6ef3899af6cd93154a2642bc674f7
Onkarabile <letlapeonka@gmail.com>
2023-03-06 13:15:36 -0800

    fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,8 +9,8 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	int i = 0;
 
-	i = 0
 	for (; n > 0; i++)
 	{
 		s[i] = b;
@@ -18,5 +18,4 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
-
 
