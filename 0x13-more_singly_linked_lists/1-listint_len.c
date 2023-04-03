a6cf45ba767fd4eec6febfb63b1a8a7a026d2d4f
Onkarabile <letlapeonka@gmail.com>
2023-04-03 13:10:24 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: a pointer to the head of the list
+ *
+ * Return: the number of elements in the list
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		node_num++;
+		h = h->next;
+	}
+
+	return (node_num);
+}
+
